ad415059024cf71e0269f5ff601c026e228a7ede
Matthew Nelson <kb0mgq@gmail.com>
2013-10-13 19:12:00 -0500

    Clean up
    Cleaned up some code

--- Firmware/azure_fiore/azure_fiore.ino
+++ Firmware/azure_fiore/azure_fiore.ino
@@ -10,9 +10,15 @@ will contain an RGB LED, several additional LEDs, and several sensors
 */
 
 /*  Notes
-A11 is the analog in for the battery level which uses a voltage divider setup for 5V
-Serial1 is used for printing to the Bluetooth module
-A13 is tied to the LED marked ON
+
+* A11 is the analog in for the battery level which uses a voltage divider setup for 5V
+
+* Serial1 is used for printing to the Bluetooth module, this connects to the phone
+
+* Serial is used as a debug port.  If the necklace is hooked up to a computer, the data is sent
+along with other debug information
+
+* A13 is tied to the LED marked ON
 
 */
 
@@ -29,8 +35,10 @@ void setup() {
   pinMode(13, OUTPUT); 
   
   //Setup our serial communications
-  //Debug port
+  
+  //Debug port through USB
   Serial.begin(19200);
+  Serial.println("Starting up Azure Fiore...");
   //Bluetooth port
   Serial1.begin(115200);
   
@@ -44,7 +52,8 @@ void loop() {
   
   byte _status;
    
-  digitalWrite(13,HIGH);  //Turns off the LED
+  digitalWrite(13,HIGH);  //Turns on the LED
+  //Read battery voltage and send the data
   read_battery();
   delay(1000);
   Serial1.println("I'm alive");
@@ -56,6 +65,7 @@ float read_battery(void) {
    // read the input on analog pin A11 and calculate the voltage
   float sensorValue = analogRead(A11)*(3.3/1024)*2;
   // print out the value on the debug port
+  Serial.print("Batt = ");
   Serial.println(sensorValue);
   Serial1.println(sensorValue);
   delay(10);     // delay in between reads for stability
